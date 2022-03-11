a145a6b2c92bdeea4a79c7411b5bb8057cb477b4
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-03-11 20:45:48 +0900

    Create Project

---
+++ src/main/java/TIAB/timebox/TimeboxApplication.java
@@ -0,0 +1,13 @@
+package TIAB.timebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TIAB/timebox/TimeboxApplicationTests.java
@@ -0,0 +1,13 @@
+package TIAB.timebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
