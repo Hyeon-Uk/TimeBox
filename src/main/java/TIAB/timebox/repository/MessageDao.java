c3a3847122059789c189eed127bcf9ddb4ef2a36
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-03-21 14:45:28 +0900

    add findByDeadline

--- src/main/java/TIAB/timebox/repository/MessageDao.java
+++ src/main/java/TIAB/timebox/repository/MessageDao.java
@@ -3,10 +3,12 @@ import TIAB.timebox.entity.User;
 import TIAB.timebox.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface MessageDao extends JpaRepository<Message,Long> {
     public Optional<List<Message>> findAllByUser(User user);
     public Optional<Message> findById(long id);
+    public Optional<List<Message>> findAllByDeadline(Date date);
 }
