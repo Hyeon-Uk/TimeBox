507fb51f4ca972b6b9251e784a7b6f7761d8551d
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-04-06 17:00:03 +0900

    enabled scheduling

--- src/main/java/TIAB/timebox/TimeboxApplication.java
+++ src/main/java/TIAB/timebox/TimeboxApplication.java
@@ -2,9 +2,10 @@ package TIAB.timebox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TimeboxApplication {
 
 	public static void main(String[] args) {
