52c85950353155a04f905f2f45f6752f98e2c107
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-07-15 22:37:01 +0900

    오타수정

--- src/main/java/TIAB/timebox/Config/LoginInterceptor.java
+++ src/main/java/TIAB/timebox/Config/LoginInterceptor.java
@@ -1,8 +1,6 @@
 package TIAB.timebox.Config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,8 +12,8 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session=request.getSession();
-        String requestUrl=request.getRequestURI();
-        if(requestUrl.equals("/auth/login")){
+        String requestURI=request.getRequestURI();
+        if(requestURI.equals("/auth/login")){
             if(session.getAttribute("id")!=null){
                 response.sendRedirect("/");
                 return false;
