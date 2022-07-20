7a1535f0aeafa5d76af129b464c66efc82f0514f
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-07-21 01:43:12 +0900

    만약 대표이메일을 바꾼 유저가있는경우를 대비해서 로직 변경

--- src/main/java/TIAB/timebox/service/security/KakaoOAuth2UserService.java
+++ src/main/java/TIAB/timebox/service/security/KakaoOAuth2UserService.java
@@ -1,7 +1,7 @@
 package TIAB.timebox.service.security;
 
 import TIAB.timebox.entity.user.User;
-import TIAB.timebox.service.user.UserService;
+import TIAB.timebox.service.user.UserServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -25,7 +26,7 @@ public class KakaoOAuth2UserService extends DefaultOAuth2UserService{
     private HttpSession httpSession;
 
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -36,11 +37,22 @@ public class KakaoOAuth2UserService extends DefaultOAuth2UserService{
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
         KakaoProfile profile=objectMapper.convertValue(attributes,KakaoProfile.class);
-        User user=new User();
-        user.setKakaoId(profile.getId());
-        user.setEmail(profile.getKakao_account().getEmail());
-        user.setImgSrc(profile.getKakao_account().getProfile().getProfile_image_url());
 
+        Optional<User> userOptional=userService.findByKakaoId(profile.getId());
+        User user;
+        if(userOptional.isPresent()) {
+            user=userOptional.get();
+            user.setKakaoId(profile.getId());
+            user.setEmail(profile.getKakao_account().getEmail());
+            user.setImgSrc(profile.getKakao_account().getProfile().getProfile_image_url());
+        }
+        else{
+            user=User.builder()
+                    .kakaoId(profile.getId())
+                    .email(profile.getKakao_account().getEmail())
+                    .imgSrc(profile.getKakao_account().getProfile().getProfile_image_url())
+                    .build();
+        }
         User savedUser=userService.save(user);
 
         httpSession.setAttribute("id",savedUser.getId());
