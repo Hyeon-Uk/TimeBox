550cdcccae363a6fd747d440fa223b3821a30659
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-07-21 01:40:38 +0900

    Service의 interface 생성 및 느슨한 연결을 위해 implements

--- src/main/java/TIAB/timebox/service/user/UserService.java
+++ src/main/java/TIAB/timebox/service/user/UserService.java
@@ -1,30 +1,13 @@
 package TIAB.timebox.service.user;
 
 import TIAB.timebox.entity.user.User;
-import TIAB.timebox.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
-public class UserService{
-    @Autowired
-    private UserRepository userRepository;
-
-    public User save(User user) {
-        User isUser=userRepository.findByKakaoId(user.getKakaoId()).orElse(null);
-        if(isUser!=null){
-            user.setId(isUser.getId());
-        }
-        return userRepository.save(user);
-    }
-
-    public User getUser(long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+public interface UserService {
+    public User save(User user);
+    public User getUser(long id);
+    public Optional<User> findByKakaoId(long kakaoId);
+    public List<User> getAllUsers();
 }
