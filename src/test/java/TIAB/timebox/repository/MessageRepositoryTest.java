12c5e581bac061ed6b14b2d4f8b8c323f0fba63c
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-07-21 20:01:53 +0900

    Update MessageRepositoryTest

--- src/test/java/TIAB/timebox/repository/MessageRepositoryTest.java
+++ src/test/java/TIAB/timebox/repository/MessageRepositoryTest.java
@@ -2,113 +2,167 @@ package TIAB.timebox.repository;
 
 import TIAB.timebox.entity.message.Message;
 import TIAB.timebox.entity.user.User;
-import TIAB.timebox.repository.MessageRepository;
-import TIAB.timebox.repository.UserRepository;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.List;
+import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MessageRepositoryTest {
-
-    @Autowired
-    private MessageRepository messageRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private MessageRepository messageRepository;
 
     User user1,user2,user3;
-    Message message1,message2,message3,message4,message5;
+    Message yesterdayMessage,message1,message2,message3,message4,message5;
 
-    @Before
+    @BeforeEach
     public void setUp(){
-        user1=new User();
-        user1.setImgSrc("img1");
-        user1.setKakaoId(1l);
-        user1.setEmail("one@naver.com");
+        user1=User.builder()
+                .imgSrc("testimg1")
+                .kakaoId(1l)
+                .email("test1@gmail.com")
+                .build();
+        user2=User.builder()
+                .imgSrc("testimg2")
+                .kakaoId(2l)
+                .email("test2@gmail.com")
+                .build();
+        user3=User.builder()
+                .imgSrc("testimg3")
+                .kakaoId(3l)
+                .email("test3@gmail.com")
+                .build();
 
-        user2=new User();
-        user2.setImgSrc("img2");
-        user2.setKakaoId(2l);
-        user2.setEmail("two@naver.com");
+        yesterdayMessage=Message.builder()
+                .content("message yesterday")
+                .width(1)
+                .height(1)
+                .deadline(new Date(new Date().getTime()-(1000*60*60*24*1)))
+                .build();
 
-        user3=new User();
-        user3.setImgSrc("img3");
-        user3.setKakaoId(3l);
-        user3.setEmail("three@naver.com");
+        message1=Message.builder()
+                .content("message1")
+                .width(1)
+                .height(1)
+                .deadline(new Date())
+                .build();
 
-        message1=new Message();
-        message1.setHeight(1);
-        message1.setWidth(1);
-        message1.setDeadline(new Date(new Date().getTime()-(1000*60*60*24*1)));//어제 날짜
-        message1.setContent("message1");
+        message2=Message.builder()
+                .content("message2")
+                .width(2)
+                .height(2)
+                .deadline(new Date())
+                .build();
 
-        message2=new Message();
-        message2.setHeight(2);
-        message2.setWidth(2);
-        message2.setDeadline(new Date());
-        message2.setContent("message2");
+        message3=Message.builder()
+                .content("message3")
+                .width(3)
+                .height(3)
+                .deadline(new Date())
+                .build();
 
-        message3=new Message();
-        message3.setHeight(3);
-        message3.setWidth(3);
-        message3.setDeadline(new Date());
-        message3.setContent("message3");
+        message4=Message.builder()
+                .content("message4")
+                .width(4)
+                .height(4)
+                .deadline(new Date())
+                .build();
 
-        message4=new Message();
-        message4.setHeight(4);
-        message4.setWidth(4);
-        message4.setDeadline(new Date());
-        message4.setContent("message4");
-
-        message5=new Message();
-        message5.setHeight(5);
-        message5.setWidth(5);
-        message5.setDeadline(new Date());
-        message5.setContent("message5");
+        message5=Message.builder()
+                .content("message5")
+                .width(5)
+                .height(5)
+                .deadline(new Date())
+                .build();
     }
-
     @Test
-    public void makeMessage(){
+    public void repositoryIsNotNull(){
+        assertThat(userRepository).isNotNull();
+        assertThat(messageRepository).isNotNull();
+    }
+    @Test
+    public void save(){
+        //given
         User savedUser=userRepository.save(user1);
-
         message1.setUser(savedUser);
-        Message savedMessage= messageRepository.save(message1);
         savedUser.getMessages().add(message1);
-        List<Message> saveMessage1= messageRepository.findAllByUser(savedUser).orElse(null);
 
-        User saveUser1=userRepository.findById(savedUser.getId()).orElse(null);
-        assertEquals(saveMessage1.size(),saveUser1.getMessages().size());
+        //when
+        Message savedMessage=messageRepository.save(message1);
+        User findUser=userRepository.findById(savedUser.getId()).orElse(null);
+
+        //then
+        assertThat(savedMessage).isEqualTo(message1);
+        assertThat(findUser.getMessages()).contains(message1);
     }
 
     @Test
-    public void getAllByDeadline(){
-        User saved=userRepository.save(user1);
-        message1.setUser(saved);//어제꺼
-        message2.setUser(saved);
-        message3.setUser(saved);
-        message4.setUser(saved);
-        message5.setUser(saved);
+    public void findById(){
+        //given
+        Message saved = messageRepository.save(message1);
 
-        messageRepository.save(message1);//어제꺼
+        //when
+        Message findMessage = messageRepository.findById(saved.getId()).orElse(null);
+
+        //then
+        assertThat(findMessage).isEqualTo(saved);
+    }
+
+    @Test
+    public void findAllByUser(){
+        //given
+        userRepository.save(user1);
+
+        message1.setUser(user1);
+        user1.getMessages().add(message1);
+        message2.setUser(user1);
+        user1.getMessages().add(message2);
+        message3.setUser(user1);
+        user1.getMessages().add(message3);
+
+        messageRepository.save(message1);
         messageRepository.save(message2);
         messageRepository.save(message3);
-        messageRepository.save(message4);
-        messageRepository.save(message5);
 
-        Date now=new Date();
-        List<Message> messages= messageRepository.findAllByDeadline(now).orElse(null);
-        assertEquals(messages.size(),4);
+
+        //when
+        List<Message> messages = messageRepository.findAllByUser(user1);
+        //then
+        assertThat(messages.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void findAllByDeadline(){
+        //given
+        userRepository.save(user1);
+        message1.setUser(user1);
+        message2.setUser(user1);
+        yesterdayMessage.setUser(user1);
+
+        messageRepository.save(message1);
+        messageRepository.save(message2);
+        messageRepository.save(yesterdayMessage);
+
+        //when
+        List<Message> todayMessages = messageRepository.findAllByDeadline(new Date());
+        List<Message> yesterdayMessages = messageRepository.findAllByDeadline(new Date(new Date().getTime() - (1000 * 60 * 60 * 24 * 1)));
+        Date yesterDay=new Date(new Date().getTime()-(1000*60*60*24*1));
+        //then
+        assertThat(todayMessages).contains(message1);
+        assertThat(todayMessages).contains(message2);
+        assertThat(yesterdayMessages).contains(yesterdayMessage);
     }
 }
