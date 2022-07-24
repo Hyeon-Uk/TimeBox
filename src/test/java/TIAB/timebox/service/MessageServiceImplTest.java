edf3a7cedb27dc769568981489b794de3c70c39e
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-07-24 18:22:32 +0900

    FileService를 Bean으로 분리한 뒤의 MessageServiceTest 코드

--- src/test/java/TIAB/timebox/service/MessageServiceImplTest.java
+++ src/test/java/TIAB/timebox/service/MessageServiceImplTest.java
@@ -8,6 +8,7 @@ import TIAB.timebox.exception.MessageNotFoundException;
 import TIAB.timebox.exception.UserNotFoundException;
 import TIAB.timebox.repository.MessageRepository;
 import TIAB.timebox.repository.UserRepository;
+import TIAB.timebox.service.file.FileService;
 import TIAB.timebox.service.message.MessageServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
@@ -27,6 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +42,8 @@ public class MessageServiceImplTest {
     private UserRepository userRepository;
     @Mock
     private MessageRepository messageRepository;
+    @Mock
+    private FileService fileService;
 
     User user1,user2;
 
@@ -58,10 +64,10 @@ public class MessageServiceImplTest {
         filename2="file2";
         filename3="file3";
         contentType="png";
-        filepath="src\\test\\resources\\messagebox";
-        filepath1=filepath+"\\"+filename1+"."+contentType;
-        filepath2=filepath+"\\"+filename2+"."+contentType;
-        filepath3=filepath+"\\"+filename3+"."+contentType;
+        filepath="/messagebox";
+        filepath1=filepath+"/"+filename1+"."+contentType;
+        filepath2=filepath+"/"+filename2+"."+contentType;
+        filepath3=filepath+"/"+filename3+"."+contentType;
 
         yesterday=new Date(new Date().getTime()-(1000 * 60 * 60 * 24 * 1));
         today=new Date();
@@ -84,8 +90,6 @@ public class MessageServiceImplTest {
         yesterdayDtoReq=MessageDtoReq.builder()
                 .user(user1)
                 .content(getMockMultifile(filename1,contentType,filepath1))
-                .filename(filename1)
-                .fileUrl(filepath+"/"+filename1+"."+contentType)
                 .deadline(yesterday)
                 .height(10)
                 .width(10)
@@ -94,8 +98,6 @@ public class MessageServiceImplTest {
         todayDtoReq=MessageDtoReq.builder()
                 .user(user1)
                 .content(getMockMultifile(filename2,contentType,filepath2))
-                .filename(filename2)
-                .fileUrl(filepath+"/"+filename2+"."+contentType)
                 .deadline(today)
                 .height(20)
                 .width(20)
@@ -104,8 +106,6 @@ public class MessageServiceImplTest {
         tomorrowDtoReq=MessageDtoReq.builder()
                 .user(user2)
                 .content(getMockMultifile(filename3,contentType,filepath3))
-                .filename(filename3)
-                .fileUrl(filepath+"/"+filename3+"."+contentType)
                 .deadline(tomorrow)
                 .height(30)
                 .width(30)
@@ -114,21 +114,35 @@ public class MessageServiceImplTest {
         yesterdayMessage=messageService.dtoToEntity(yesterdayDtoReq);
         yesterdayMessageAfter=messageService.dtoToEntity(yesterdayDtoReq);
         yesterdayMessageAfter.setId(1l);
+        yesterdayMessageAfter.setFilename(filename1);
+        yesterdayMessageAfter.setFileUrl(filepath+"/"+filename1+"."+contentType);
 
         todayMessage=messageService.dtoToEntity(todayDtoReq);
         todayMessageAfter=messageService.dtoToEntity(todayDtoReq);
         todayMessageAfter.setId(2l);
+        todayMessageAfter.setFilename(filename2);
+        todayMessageAfter.setFileUrl(filepath+"/"+filename2+"."+contentType);
 
         tomorrowMessage=messageService.dtoToEntity(tomorrowDtoReq);
         tomorrowMessageAfter=messageService.dtoToEntity(tomorrowDtoReq);
         tomorrowMessageAfter.setId(2l);
+        tomorrowMessageAfter.setFilename(filename3);
+        tomorrowMessageAfter.setFileUrl(filepath+"/"+filename3+"."+contentType);
 
         yesterdayDtoRes=messageService.entityToDto(yesterdayMessage);
+        yesterdayDtoRes.setFilename(filename1);
+        yesterdayDtoRes.setFileUrl(filepath1);
+
         todayDtoRes=messageService.entityToDto(todayMessage);
+        todayDtoRes.setFilename(filename2);
+        todayDtoRes.setFileUrl(filepath2);
+
         tomorrowDtoRes=messageService.entityToDto(tomorrowMessage);
+        tomorrowDtoRes.setFilename(filename3);
+        tomorrowDtoRes.setFileUrl(filepath3);
     }
 
-    private void assertEqualDtoAndDto(MessageDtoRes dto1,MessageDtoRes dto2){
+    private void assertEqualDtoRes(MessageDtoRes dto1,MessageDtoRes dto2){
         assertThat(dto1.getDeadline()).isEqualTo(dto2.getDeadline());
         assertThat(dto1.getFilename()).isEqualTo(dto2.getFilename());
         assertThat(dto1.getFileUrl()).isEqualTo(dto2.getFileUrl());
@@ -148,7 +162,7 @@ public class MessageServiceImplTest {
             MessageDtoRes actual = messageService.getByMessageId(todayMessageAfter.getId());
 
             //then
-            assertEqualDtoAndDto(actual,todayDtoRes);
+            assertEqualDtoRes(actual,todayDtoRes);
         }
 
         @Test
@@ -156,12 +170,21 @@ public class MessageServiceImplTest {
             //given
             when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user1));
             when(messageRepository.save(any())).thenReturn(todayMessageAfter);
+            doAnswer(new Answer() {
+                @Override
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    MessageDtoReq dto=(MessageDtoReq) invocation.getArgument(0);
+                    dto.setFilename(filename2);
+                    dto.setFileUrl(filepath2);
+                    return dto;
+                }
+            }).when(fileService).save(any());
 
             //when
             MessageDtoRes actual = messageService.save(user1.getId(), todayDtoReq);
 
             //then
-            assertEqualDtoAndDto(actual,todayDtoRes);
+            assertEqualDtoRes(actual,todayDtoRes);
         }
     }
 
@@ -179,7 +202,7 @@ public class MessageServiceImplTest {
         @Test
         public void saveFailed_사용자없음(){
             //given
-            when(userRepository.findById(3l)).thenThrow(UserNotFoundException.class);
+            when(userRepository.findById(anyLong())).thenThrow(UserNotFoundException.class);
             //when & then
             assertThrows(UserNotFoundException.class,()->messageService.save(user1.getId(),todayDtoReq));
         }
