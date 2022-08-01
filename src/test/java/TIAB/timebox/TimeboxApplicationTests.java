f91269d5340c0bfe8a8163658fc70fc31aec75c8
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-08-01 22:59:25 +0900

    메인 메서드 테스트코드

--- src/test/java/TIAB/timebox/TimeboxApplicationTests.java
+++ src/test/java/TIAB/timebox/TimeboxApplicationTests.java
@@ -1,14 +1,15 @@
 package TIAB.timebox;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.persistence.EntityManager;
 
 @SpringBootTest
 class TimeboxApplicationTests {
 	@Test
+	@DisplayName("Main Class Test")
 	void contextLoads() {
+		TimeboxApplication.main(new String[]{});
 	}
 
 }
