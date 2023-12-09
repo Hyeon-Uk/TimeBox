b01f4153d862a439c0b4ed63a762ee7fdbbac6b8
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-12-10 01:02:12 +0900

    [Feat] Modify according to refactored code

--- src/main/java/TIAB/timebox/controller/HomeController.java
+++ src/main/java/TIAB/timebox/controller/HomeController.java
@@ -1,11 +1,11 @@
 package TIAB.timebox.controller;
 
-import TIAB.timebox.dto.MemberDtoRes;
+import TIAB.timebox.dto.MemberDTO;
 import TIAB.timebox.service.member.MemberService;
+import TIAB.timebox.service.security.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +18,11 @@ public class HomeController {
     private final MemberService memberService;
 
     @GetMapping
-    public String home(@AuthenticationPrincipal OAuth2User oAuth2User, Model model) {
-        long id = oAuth2User.getAttribute("id");
-        MemberDtoRes member = memberService.getMember(id);
-        model.addAttribute("memberDto", member);
+    public String home(@AuthenticationPrincipal CustomOAuth2User oAuth2User, Model model) {
+        MemberDTO.Response member = memberService.findById(MemberDTO.Request.builder()
+                .id(oAuth2User.getId())
+                .build());
+        model.addAttribute("member", member);
         return "home";
     }
 }
